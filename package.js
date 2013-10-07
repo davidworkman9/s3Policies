2fe8a25bc3d391542bff46584775bf14cd996fba
Dave Workman <davidworkman9@gmail.com>
2013-10-07 15:33:27 -0400

    Working!

--- package.js
+++ package.js
@@ -2,8 +2,6 @@ Package.describe({
     summary: "Generate S3 upload and download policies"
 });
 
-Npm.depends({ crypto: '0.0.3' });
-
 Package.on_use(function (api) {
     api.export('s3Policies');
     api.add_files([
