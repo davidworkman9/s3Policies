21ef591213e92abe636c0c7bfdd42077668b401d
Dave Workman <davidworkman9@gmail.com>
2014-09-08 09:13:22 -0400

    updated to 0.9.0

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-    summary: "Generate S3 upload and download policies"
+    name: "mrt:meteor-editable",
+    summary: "Generate S3 upload and download policies",
+    version: "0.0.5",
+    git: "https://github.com/davidworkman9/s3Policies.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
     api.export('s3Policies');
     api.add_files([
         's3Policy.js'
